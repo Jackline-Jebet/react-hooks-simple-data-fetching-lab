20d406ca35c2e83cb5a52e4fe3cca76cd28acb2e
Jackline Jebet <jebetjackline2016@gmail.com>
2022-07-17 18:20:33 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useEffect, useState } from "react"
+
+
+function App() {
+    const [dogPc, setDogPc] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => { 
+            setDogPc(data.message);
+    });
+}, []);
+
+if (!dogPc)
+return <p>Loading...</p>
+
+return <img src={dogPc} alt="A Random Dog" />;
+
+}
+
+export default App;
